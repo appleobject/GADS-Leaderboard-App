9ce9c4872d6b5fa26d671d009341594447fdcf45
appleobject <52360868+appleobject@users.noreply.github.com>
2020-08-30 09:27:27 +0100

    Type: feat
    Add ServiceBuilder.
    
    The ServiceBuilder object make a call to the buildService method passing the interface with the http functions as an argument then the value return from this method is used to make a network request to the endpoint using the enqueue method from retrofit.

--- app/src/main/java/com/appleobject/leaderboard/MainActivity.kt
+++ app/src/main/java/com/appleobject/leaderboard/MainActivity.kt
@@ -2,10 +2,46 @@ package com.appleobject.leaderboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.appleobject.leaderboard.helper.toast
+import com.appleobject.leaderboard.model.Learners
+import com.appleobject.leaderboard.retrofit.LearnersRetrofit
+import com.appleobject.leaderboard.retrofit.ServiceBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loadHours()
+    }
+
+    private fun loadHours() {
+        tvHello.setOnClickListener {
+            val getLearners = ServiceBuilder.buildService(LearnersRetrofit::class.java)
+            val requestCall = getLearners.get()
+
+            requestCall.enqueue(object :Callback<List<Learners>>{
+                override fun onResponse(
+                    call: Call<List<Learners>>,
+                    response: Response<List<Learners>>
+                ) {
+                    if (response.isSuccessful){
+                        val response = response.body()
+                        tvHello.text = response.toString()
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Learners>>, t: Throwable) {
+                    toast("Error occurred : $t")
+                }
+
+            })
+        }
     }
 }
---
+++ app/src/main/java/com/appleobject/leaderboard/model/Learners.kt
@@ -0,0 +1,9 @@
+package com.appleobject.leaderboard.model
+
+data class Learners(
+    private var name: String,
+    private var hours: Int,
+    private var country: String,
+    private var badgeUrl: String
+) {
+}
